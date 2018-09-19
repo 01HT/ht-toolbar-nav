4925bed3e128e1bac50c7f0655e5fca1ef68bacd
maksimyurkov <yurkov.maksim.sergeevich@gmail.com>
2018-09-19 13:43:33 +0300

    changes in syntax

--- ht-toolbar-nav.js
+++ ht-toolbar-nav.js
@@ -1,14 +1,25 @@
 "use strict";
 import { LitElement, html } from "@polymer/lit-element";
-import { repeat } from "lit-html/lib/repeat.js";
+import { repeat } from "lit-html/directives/repeat.js";
+import "@polymer/iron-iconset-svg";
+import "@polymer/iron-icon";
+
 class HTToolbarNav extends LitElement {
-  _render({ data, page }) {
+  render() {
+    const { data, page } = this;
     return html`
     <style>
       :host {
           display: block;
           position: relative;
           box-sizing: border-box;
+      }
+
+      iron-icon {
+        width: 18px;
+        height: 18px;
+        margin-left: 4px;
+        color: var(--secondary-text-color);
       }
 
       a {
@@ -51,14 +62,23 @@ class HTToolbarNav extends LitElement {
         background: #dfe1e5;
       }
     </style>
+        <iron-iconset-svg size="24" name="ht-toolbar-nav">
+        <svg>
+            <defs>
+                <g id="open-in-new"><path d="M19 19H5V5h7V3H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2v-7h-2v7zM14 3v2h3.59l-9.83 9.83 1.41 1.41L19 6.41V10h2V3h-7z"></path></g>
+            </defs>
+        </svg>
+    </iron-iconset-svg>
     <nav role="navigation">
       ${repeat(
         data,
         i =>
-          html`<a href=${i.href} active?=${
+          html`<a href=${i.href} target=${i.blank ? "_blank" : ""} ?active=${
             i.href && i.href.startsWith(`/${page}`) ? true : false
-          }>${
-            i.title
+          }>${i.title} ${
+            i.blank
+              ? html`<iron-icon icon="ht-toolbar-nav:open-in-new"></iron-icon>`
+              : ``
           }<div class="hover-underline"></div><div class="active-underline"></div></a>`
       )}
     </nav>
@@ -71,14 +91,9 @@ class HTToolbarNav extends LitElement {
 
   static get properties() {
     return {
-      data: Array,
-      page: String
+      data: { type: Array },
+      page: { type: String }
     };
-  }
-
-  constructor() {
-    super();
-    this.data = [];
   }
 }
 
